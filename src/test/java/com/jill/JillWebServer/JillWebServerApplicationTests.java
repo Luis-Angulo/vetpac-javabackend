f2710f571402c88c08617206870df0b1d3e0f3a1
Luis Angulo <luisf117@gmail.com>
2021-03-01 06:36:13 -0700

    initial commit for valentine

---
+++ src/test/java/com/jill/JillWebServer/JillWebServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jill.JillWebServer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JillWebServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
